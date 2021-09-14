5be41abdf1f0df5ae19318831605fabaaf088be3
Rdsarkar <rajapscl@gmail.com>
2021-09-14 10:57:45 +0600

    first Push
    this contain Oracle database and Controller with scaffolding

---
+++ ColorOracle/Models/Color.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ColorOracle.Models
+{
+    public partial class Color
+    {
+        public decimal? ColorId { get; set; }
+        public string ColorName { get; set; }
+    }
+}
